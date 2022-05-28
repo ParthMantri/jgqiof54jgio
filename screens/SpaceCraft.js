3f93d6eff7925cd0be918f21d1158dc75244d99b
ParthMantri <87506255+ParthMantri@users.noreply.github.com>
2022-05-28 15:36:26 -0400

    Add files via upload

---
+++ screens/SpaceCraft.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { 
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+export default class SpaceCraftScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>SpaceCraft Screen!</Text>
+            </View>
+        )
+    }
+}
